5a977ccc2feedc9d54f59fc3197cc02c01996153
PrashantPivison <prashant.o@pivisions.com>
2025-12-30 16:58:54 +0530

    Update API_BASE_URL to use ngrok URL for development

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -8,7 +8,7 @@ import authService from './authService';
  * - Prod: https://api.yourdomain.com
  */
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:5000';
+  import.meta.env.VITE_API_URL || ' https://71b75c479442.ngrok-free.app/api';
 
 /**
  * Axios instance
